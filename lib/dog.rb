bb3301345726b6902cbc55be96907f08a9ceac5e
Zachary Farrell <zfarrell0507@gmail.com>
2022-09-05 14:51:30 -0400

    commit tree 849274ed62be4a8fe868ed69e4cc311fddd7914b

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+    def bark
+        puts "Woof!"
+    end
+    def sit
+        puts "The Dog is sitting"
+    end
+end
+
+fido = Dog.new
+fido.bark
+
+snoopy = Dog.new
+snoopy.bark
+snoopy.sit
